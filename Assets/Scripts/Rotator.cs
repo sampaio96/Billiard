5c0570ba455f05fae28c0e1602b6ef67c004475f
sampaio96 <gsampaio@wesleyan.edu>
2016-10-25 18:08:25 +0100

    Set Pickups

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public float speedOfOscillation;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (-30,-30,30) * Time.deltaTime);
+
+		float oscillationAmplitude = Mathf.Sin (Time.time);
+
+		// transform.Translate (new Vector3 (0, speedOfOscillation * oscillationAmplitude, 0) * Time.deltaTime);
+	}
+}

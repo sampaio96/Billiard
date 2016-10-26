65265d8442fb3a6dd950609211330bf3fd52c35d
sampaio96 <gsampaio@wesleyan.edu>
2016-10-26 01:36:09 +0100

    built!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
+	private int count;
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	// called before rendering a frame. most game code comes here.
@@ -24,6 +31,22 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce (movement*speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pickup"))
+		{
+			other.gameObject.SetActive (false);
+			count += 1;
+			SetCountText ();
+			if (count == 8) {
+				winText.text = "You won!";
+			}
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
 	}
 
 }
